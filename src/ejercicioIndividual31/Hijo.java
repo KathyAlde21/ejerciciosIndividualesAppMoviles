4048341390d213b73c58269a514db17e1b97b247
Katherine Alderete Gomez <katherinealdereteg@gmail.com>
2024-10-09 18:10:22 -0300

    ejercicio 31

---
+++ src/ejercicioIndividual31/Hijo.java
@@ -0,0 +1,37 @@
+
+package ejercicioIndividual31;
+
+public class Hijo extends CargaFamiliar {
+    
+    private String parentesco2;
+    
+    public Hijo () {};
+
+    public Hijo (
+            String parentesco2,
+            String runCarga,
+            String nombreCarga,
+            String apellidoMaternoCf,
+            String apellidoPaternoCf,
+            String parentesco,
+            int edadCarga) {
+        super(runCarga, nombreCarga, apellidoMaternoCf, apellidoPaternoCf, parentesco, edadCarga);
+        this.parentesco2 = parentesco2;
+    }
+
+    public String getParentesco2() {
+        return parentesco2;
+    }
+
+    public void setParentesco2(String parentesco2) {
+        this.parentesco2 = parentesco2;
+    }
+
+    @Override
+    public String toString() {
+        return "Otro{" + "parentesco2=" + parentesco2
+                + '}'
+                + super.toString();
+    }
+    
+}
---
+++ src/ejercicioIndividual31/Honorario.java
@@ -0,0 +1,64 @@
+
+package ejercicioIndividual31;
+
+public class Honorario extends Trabajador{
+    
+    private String giro;
+    private String anoInicioActividades;
+    
+    public Honorario (){};
+
+    public Honorario(
+            String giro,
+            String anoInicioActividades,
+            String primerNombre,
+            String segundoNombre,
+            String apellidoMaterno,
+            String apellidoPaterno,
+            String run,
+            String telefono,
+            int edad
+    ) {
+        super(primerNombre, segundoNombre, apellidoMaterno, apellidoPaterno,
+                run, telefono, edad);
+        this.giro = giro;
+        this.anoInicioActividades = anoInicioActividades;
+    }
+
+    public String getGiro() {
+        return giro;
+    }
+
+    public void setGiro(String giro) {
+        this.giro = giro;
+    }
+
+    public String getAnoInicioActividades() {
+        return anoInicioActividades;
+    }
+
+    public void setAnoInicioActividades(String anoInicioActividades) {
+        this.anoInicioActividades = anoInicioActividades;
+    }
+
+    //*****************************
+    
+    @Override
+    public void mostrarDatos(){
+        System.out.println("Nombre: " + getPrimerNombre() 
+                + " " + getSegundoNombre() + " " + getApellidoMaterno()
+                + " " + getApellidoPaterno() + " RUN: " + getRun()
+                + " Telefono: " + getTelefono()
+                + " Edad: " + getEdad() + "Giro laboral: " + giro
+                + "Ano de Inicio de Actividades: " + anoInicioActividades);
+    }
+    
+    @Override
+    public String toString() {
+        return "Honorario{" + "giro=" + giro
+                + " anoInicioActividades=" + anoInicioActividades
+                + '}'
+                + super.toString();
+    }
+     
+}
